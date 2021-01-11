5fe2d6bf653af55ca7fc9ebb7d2a4b6edbb0f959
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-11 08:54:50 -0600

    Fetched To Dos from backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import $ from "jquery"
-import _ from "lodash"
+import $ from "jquery";
+import _ from "lodash";
 
-$('body').append('<h1>Hello World</h1>')
+// Adding the initial HTML to the body
+$("body").append(`<h1>The Todo App</h1>
+<hr>
+<h2>The Todos</h2>
+<ul id="todolist">
 
+</ul>
+`);
 
+// The UL for the To Do list
+const $todoList = $("#todolist");
+const baseUrl = "http://localhost:3000/todos";
 
+// Function to grab todos from backend
+const fetchTodos = async () => {
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  // Return promise of data
+  return data;
+};
 
-
+// Render todos to DOM
